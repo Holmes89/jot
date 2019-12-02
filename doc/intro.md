dda5a718e0b8f2f73b7b153c4b56549451abc0ce
Joel Holmes <holmes89@gmail.com>
2019-12-01 20:39:06 -0500

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/jot/core_test.clj
@@ -0,0 +1,7 @@
+(ns jot.core-test
+  (:require [clojure.test :refer :all]
+            [jot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
