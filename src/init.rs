dcfe3a143019d88a23159704faa1a8e5a9f065a5
Joel Holmes <jholmes@ecr.ai>
2020-01-27 14:57:50 -0500

    init file

---
+++ src/init.rs
@@ -0,0 +1,55 @@
+extern crate rustyline;
+
+use git2::Repository;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+use std::path::Path;
+
+pub fn intialize() {
+    let mut rl = Editor::<()>::new();
+    let readline = rl.readline("Remote Git URL >> ");
+    match readline {
+        Ok(url) => println!("{}", clone_repo(&url)),
+        Err(ReadlineError::Interrupted) => {
+            println!("CTRL-C");
+        }
+        Err(ReadlineError::Eof) => {
+            println!("CTRL-D");
+        }
+        Err(err) => {
+            println!("Error: {:?}", err);
+        }
+    }
+}
+
+fn clone_repo(url: &str) -> String {
+    let home = std::env::home_dir().unwrap();
+    let jot_home = format!("{}{}", home.to_str().unwrap(), "/.jot");
+    let pub_path = format!("{}{}", home.to_str().unwrap(), "/.ssh/id_rsa.pub");
+    let private_path = format!("{}{}", home.to_str().unwrap(), "/.ssh/id_rsa");
+    let p = Path::new(&jot_home);
+    if p.exists() {
+        panic!("directory already exists")
+    }
+
+    let mut repo_builder = git2::build::RepoBuilder::new();
+    let mut fetch_options = git2::FetchOptions::new();
+    let mut auth_callback = git2::RemoteCallbacks::new();
+
+    auth_callback.credentials(|_, _, _| {
+        let credentials = git2::Cred::ssh_key(
+            "git",
+            Some(Path::new(&pub_path)),
+            Path::new(&private_path),
+            None,
+        )
+        .expect("Could not create credentials object");
+
+        Ok(credentials)
+    });
+
+    fetch_options.remote_callbacks(auth_callback);
+    repo_builder.fetch_options(fetch_options);
+    let _repo = repo_builder.clone(url, p).unwrap();
+    String::from("initialized")
+}
