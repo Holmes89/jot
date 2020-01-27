406165190c9d0e19d62300f9d0fab7230bfa1d19
Joel Holmes <jholmes@ecr.ai>
2020-01-27 08:22:40 -0500

    restructured

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 extern crate clap;
-use clap::{App, Arg};
+use clap::App;
 
 fn main() {
-    let _matches = App::new("Jot")
+    let matches = App::new("Jot")
         .version("0.2")
         .author("Joel Holmes <holmes89@gmail.com>")
         .about("Create quick notes")
-        .arg(Arg::with_name("create").short("c").long("create"))
+        .subcommand(
+            App::new("create").about("create new entry"), //TODO add functionality for "collections"
+        )
+        .subcommand(App::new("edit").about("edit existing entry")) //TODO default to today, otherwise allow for edit entry date or collection
+        .subcommand(App::new("read").about("read entry")) //TODO default to today, otherwise allow for read entry date or collection
+        .subcommand(App::new("init").about("initialize app")) // initialize application
         .get_matches();
+
+    match matches.subcommand_name() {
+        Some("create") => println!("created"),
+        Some("edit") => println!("edited"),
+        Some("read") => println!("read"),
+        Some("init") => println!("init'd"),
+        None => println!("HALP!"),
+        _ => unreachable!(),
+    }
 }
